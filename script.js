343309408c3da2107c190626df471a9199444556
kesia-salgado <kesia.salgado@gmail.com>
2023-07-09 20:18:39 -0300

    adicionando funções .js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// funcção utilizada para animação digitando na plataforma//
+// função utilizada para animação digitando na plataforma//
 function ativaLetra(elemento){
     const arrayTexto = elemento.innerHTML.split(''); /*o espaçamento de letras pelo inner.html.split só se dá com os parentes vazios*/
     elemento.innerHTML = '';
@@ -12,3 +12,18 @@ function ativaLetra(elemento){
 const titulo = document.querySelector('.digitando');
 ativaLetra(titulo);/*Para que o efeito possa funcionar, o console.log que aqui está representado
 pelo ativaletra,precisa estar abaixo do comando global representado por const titulo*/
+
+/* função para sobrepor texto quando o mouse passar em cima de cada icone*/
+
+function showTooltip(element, text) {
+    const tooltip = document.getElementById('tooltip');
+    tooltip.textContent = text;
+    tooltip.style.display = 'block';
+    tooltip.style.top = element.offsetTop + element.offsetHeight + 'px';
+    tooltip.style.left = element.offsetLeft + 'px';
+  }
+
+  function hideTooltip() {
+    const tooltip = document.getElementById('tooltip');
+    tooltip.style.display = 'none';
+  }
